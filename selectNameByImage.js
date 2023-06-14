5f2f4d7131b0fdae0d60a4191d8103be393e8a3f
Takacs, Zsolt (ext) <zsolt.takacs.ext@siemens-energy.com>
2023-06-14 12:14:29 +0200

    Add practice mistakes option
    gameType='all' -- default
    gameType='mistakesOnly' -- practice

--- selectNameByImage.js
+++ selectNameByImage.js
@@ -11,6 +11,7 @@ speciesImport.then(data => {
             return {
                 title: 'Magyarország halai',
                 speciesList: data.default,
+                puzzleItems: data.default,
                 puzzle: null,
                 incorrect1: '',
                 incorrect2: '',
@@ -26,12 +27,17 @@ speciesImport.then(data => {
                 choices: [],
                 choiceIsRight: [false, false, false, false],
                 choiceIsWrong: [false, false, false, false],
-                isAnswered: false
+                isAnswered: false,
+                mistakeIndexes: [],
+                gameType: 'all'
             }
         },
         computed: {
             totalSpeciesCount() {
                 return this.speciesList.length;
+            },
+            totalPuzzleItemsCount() {
+                return this.puzzleItems.length;
             },
             puzzleName() {
                 return this.puzzle.Name;
@@ -59,23 +65,35 @@ speciesImport.then(data => {
             },
             gameTitle() {
                 if (this.game === "SelectNameByImage") {
-                    return "Melyik halat látod a képen?";
+                    if (this.gameType === 'all') {
+                        return "Melyik halat látod a képen?";
+                    } else {
+                        return "Hibásak gyakorlása: Melyik halat látod a képen?";
+                    }
                 }
             }
         },
         methods: {
             getRandomSpecies() {
-                if (this.previousPuzzleIDs.length == this.speciesList.length) { return this.puzzle; }
+                if (this.previousPuzzleIDs.length == this.puzzleItems.length) { return this.puzzle; }
 
                 do {
-                    var candidate = this.speciesList[Math.floor(Math.random() * this.speciesList.length)];
+                    var candidate = this.puzzleItems[Math.floor(Math.random() * this.puzzleItems.length)];
+                    console.log('candidate: ' + candidate.ID + '-' + candidate.Name);
                 } while (this.previousPuzzleIDs.some((id) => candidate.ID === id));
 
                 this.previousPuzzleIDs.push(candidate.ID);
+                console.log('added to previous puzzle ids')
 
                 return candidate;
             },
-            getRandomSpeciesExceptIDs(ids) {
+            getRandomSpeciesFromPuzzleItemsExceptIDs(ids) {
+                do {
+                    var result = this.puzzleItems[Math.floor(Math.random() * this.puzzleItems.length)];
+                } while (ids.some((id) => result.ID === id))
+                return result;
+            },
+            getRandomSpeciesFromAllItemsExceptIDs(ids) {
                 do {
                     var result = this.speciesList[Math.floor(Math.random() * this.speciesList.length)];
                 } while (ids.some((id) => result.ID === id))
@@ -87,22 +105,25 @@ speciesImport.then(data => {
                     this.incorrect1 = this.speciesList.find((item) => item.ID === this.puzzle.SimilarSpecies[0]);
                 }
                 else {
-                    this.incorrect1 = this.getRandomSpeciesExceptIDs([this.puzzle.ID]);
+                    this.incorrect1 = this.getRandomSpeciesFromAllItemsExceptIDs([this.puzzle.ID]);
                 }
+                console.log('got incorrect1 ' + this.incorrect1.ID + ' - ' + this.incorrect1.Name);
 
                 if (this.puzzle.SimilarSpecies.length >= 2) {
                     this.incorrect2 = this.speciesList.find((item) => item.ID === this.puzzle.SimilarSpecies[1]);
                 }
                 else {
-                    this.incorrect2 = this.getRandomSpeciesExceptIDs([this.puzzle.ID, this.incorrect1.ID]);
+                    this.incorrect2 = this.getRandomSpeciesFromAllItemsExceptIDs([this.puzzle.ID, this.incorrect1.ID]);
                 }
+                console.log('got incorrect2 ' + this.incorrect2.ID + ' - ' + this.incorrect2.Name);
 
                 if (this.puzzle.SimilarSpecies.length >= 3) {
                     this.incorrect3 = this.speciesList.find((item) => item.ID === this.puzzle.SimilarSpecies[2]);
                 }
                 else {
-                    this.incorrect3 = this.getRandomSpeciesExceptIDs([this.puzzle.ID, this.incorrect1.ID, this.incorrect2.ID]);
+                    this.incorrect3 = this.getRandomSpeciesFromAllItemsExceptIDs([this.puzzle.ID, this.incorrect1.ID, this.incorrect2.ID]);
                 }
+                console.log('got incorrect3 ' + this.incorrect3.ID + ' - ' + this.incorrect3.Name);
             },
             shuffle(array) {
                 let currentIndex = array.length, randomIndex;
@@ -136,14 +157,20 @@ speciesImport.then(data => {
                 }
                 else {
                     this.comment = '<i>' + this.puzzle.Category + '</i>';
+                    console.log('comment added');
                     this.choiceIsWrong[indexOfAnswer] = true;
+                    console.log('choice is wrong');
+                    this.mistakeIndexes.push(this.puzzle.ID);
+                    console.log('mistake pushed');
                     var indexOfCorrectAnswer = this.choices.indexOf(this.puzzleName);
+                    console.log('indexOfCorrectAnswer');
                     this.choiceIsRight[indexOfCorrectAnswer] = true;
+                    console.log('set correct as green');
                 }
                 ++this.numberOfAnsweredQuestions;
 
                 setTimeout(() => {
-                    if (this.previousPuzzleIDs.length >= this.totalSpeciesCount) {
+                    if (this.previousPuzzleIDs.length >= this.totalPuzzleItemsCount) {
                         this.endGame();
                     }
                     else {
@@ -175,10 +202,19 @@ speciesImport.then(data => {
                 this.isAnswered = false;
             },
             reset() {
+                this.gameType = 'all';
+                this.puzzleItems = this.speciesList;
                 this.startQuiz()
+                this.mistakeIndexes = [];
+            },
+            restartWithMistakes() {
+                this.gameType = 'mistakesOnly';
+                this.puzzleItems = this.speciesList.filter((item) => this.mistakeIndexes.includes(item.ID));
+                this.startQuiz()
+                this.mistakeIndexes = [];
             },
             endGame() {
-                this.comment += "<br/>Vége a játéknak.";
+                this.comment = "Vége a játéknak! Eredmény: " + Math.floor(this.totalPoints / this.numberOfAnsweredQuestions * 100) + '%';
             },
             randomizeChoices() {
                 this.choices = this.shuffle([this.puzzleName, this.incorrect1.Name, this.incorrect2.Name, this.incorrect3.Name]);
